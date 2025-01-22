fe52335c13e5cee72a0a5f1d655f707d7b301beb
martin <martin@gmail.com>
2025-01-22 15:13:46 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    float tarifa = 0, distancia = 0, costo, pago, cambio, t1 = 0, d1 = 0;
+    int opc, opc2;
+    int c1, c2, c5, ce;
+
+    do
+    {
+        printf("1. Ingresar distancia en km\n");
+        printf("2. Ingresar tarifa por km\n");
+        printf("3. Calculo costo boleto\n");
+        printf("4. Calculo cambio\n");
+        printf(">> ");
+        scanf("%d", &opc2);
+
+        switch (opc2)
+        {
+        case 1:
+            do
+            {
+                printf("Ingrese la distancia en km: ");
+                scanf("%f", &distancia);
+
+                if (distancia <= 0)
+                {
+                    printf("ERROR. Debe ingresar un valor superior a cero\n");
+                }
+            } while (distancia <= 0);
+
+            d1 = distancia;
+            break;
+
+        case 2:
+            do
+            {
+                printf("Ingrese la tarifa (en pesos) por km: ");
+                scanf("%f", &tarifa);
+
+                if (tarifa <= 0)
+                {
+                    printf("ERROR. Debe ingresar un valor superior a cero\n");
+                }
+            } while (tarifa <= 0);
+
+            t1 = tarifa;
+            break;
+
+        case 3:
+            distancia = d1;
+            tarifa = t1;
+
+            if (distancia == 0 || tarifa == 0)
+            {
+                printf("ERROR. Primero debe ingresar la distancia y tarifa\n");
+            }
+            else
+            {
+                costo = distancia * tarifa;
+
+                printf("El costo del boleto es: %.2f\n", costo);
+            }
+            break;
+
+        case 4:
+            if (distancia == 0 || tarifa == 0)
+            {
+                printf("ERROR. Primero debe ingresar la distancia y tarifa\n");
+            }
+            else
+            {
+                do
+                {
+                    printf("Ingrese el monto total pagado: ");
+                    scanf("%f", &pago);
+
+                    if (pago < costo)
+                    {
+                        printf("ERROR. Debe ingresar un valor igual o superior al costo\n");
+                    }
+                } while (pago < costo);
+
+                cambio = pago - costo;
+
+                if (pago > costo)
+                {
+                    ce = cambio + 0.5;
+                    c5 = ce / 5;
+                    ce %= 5;
+                    c2 = ce / 2;
+                    ce %= 2;
+                    c1 = ce;
+
+                    printf("Su cambio es de: %.2f\n", cambio);
+                    printf("El cambio se entrega con %d monedas de 5, %d monedas de 2 y %d monedas de 1\n", c5, c2, c1);
+                }
+
+                if (pago == costo)
+                {
+                    printf("Su cambio es de: %.2f\n", cambio);
+                }
+            }
+            break;
+
+        default:
+            printf("ERROR. Debe escoger una de las 4 opciones\n");
+            break;
+        }
+
+        printf("Desea ingresar otra opcion?\n");
+        printf("1. Si\n");
+        printf("2. No\n");
+        printf(">> ");
+        scanf("%d", &opc);
+    } while (opc == 1);
+
+    printf("RESUMEN\n");
+    printf("El costo del boleto es: %.2f\n", costo);
+    printf("El monto pagado es: %.2f\n", pago);
+    printf("El cambio entregado es: %.2f\n", cambio);
+    
+    return 0;
+    }
